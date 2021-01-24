1284d3aff8a71ca598ec39c378a23948f4baa87f
Shikhar Singh <shikharsingh0076@gmail.com>
2021-01-24 18:14:09 +0530

    Footer

--- src/components/footer/FooterComponent.tsx
+++ src/components/footer/FooterComponent.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export function Footer() {
-    return (
-        <div>
-            Footer
+    return(
+    <div className="footer jumbotron">
+        <div className="container">
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                    <p>© Copyright 2020 Weather Erudite. All Rights Reserved.</p>
+                </div>
+            </div>
         </div>
-    );
+    </div>
+    )
 }
