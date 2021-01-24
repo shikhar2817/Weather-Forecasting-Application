e3fb5373709162fec576d3ec2d2438a8b29ca147
Shikhar Singh <shikharsingh0076@gmail.com>
2021-01-24 20:46:27 +0530

    Search Bar Decoration

--- src/components/search/SearchComponent.tsx
+++ src/components/search/SearchComponent.tsx
@@ -4,28 +4,29 @@ export default function Search(){
     const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
     return (
         <>
-            <div className="container">
-                <h2></h2>
-                <InputGroup>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col">
-                                <Input icon="search" placeholder="Search Location, Postal"/>
-                            </div>
-                            <div className="ml-0">
-                                <InputGroupAddon addonType="append">
-                                    <Button color="secondary"> <i className="fa fa-map-marker" aria-hidden="true"></i></Button>
-                                </InputGroupAddon>
-                            </div>
-                            <div className="ml-1">
-                                <InputGroupAddon addonType="append">
-                                    <Button color="success"> <i className="fa fa-search "> Search</i> </Button>  
-                                </InputGroupAddon>
+            <div style={{ backgroundImage: "url(images/weather.jpg)", width:"100%", height:"200px" }}>
+                <div className="container" >
+                    <br/><br/><br/>
+                    <InputGroup>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col">
+                                    <Input icon="search" placeholder="Search Location, Postal"/>
+                                </div>
+                                <div className="ml-0">
+                                    <InputGroupAddon addonType="append">
+                                        <Button color="secondary"> <i className="fa fa-map-marker" aria-hidden="true"></i></Button>
+                                    </InputGroupAddon>
+                                </div>
+                                <div className="ml-1">
+                                    <InputGroupAddon addonType="append">
+                                        <Button color="success"> <i className="fa fa-search "> Search</i> </Button>  
+                                    </InputGroupAddon>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </InputGroup>
-                <h2></h2>
+                    </InputGroup>
+                </div>
             </div>
         </>
     );
