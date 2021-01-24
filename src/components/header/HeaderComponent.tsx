1e5e248378cefab58901944acd03a78da6f51235
Shikhar Singh <shikharsingh0076@gmail.com>
2021-01-24 17:02:28 +0530

    Header

--- src/components/header/HeaderComponent.tsx
+++ src/components/header/HeaderComponent.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
 
-export function Header () {
+
+export function Header(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
-        <div>
-            HEADER
-        </div>
+        <>
+        <Navbar color="dark" dark expand="md">
+            <div className="container">
+                <NavbarToggler onClick={toggle} />
+                <NavbarBrand className="mr-40 ml-3" href="/">
+                    <img src="images/logo.png" height="30"  alt='Weather Erudite'/>
+                </NavbarBrand>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem> 
+                            <NavLink className="nav-link" to="/home">
+                                <span className="fa fa-home fa-lg"></span> Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem> 
+                            <NavLink className="nav-link" to="/aboutus">
+                                <span className="fa fa-info fa-lg"></span> Aboutus
+                            </NavLink>
+                        </NavItem>
+
+                        <NavItem> 
+                            <NavLink className="nav-link" to="/contactus">
+                                <span className="fa fa-address-card fa-lg"></span> ContactUs
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                        </Collapse>
+            </div>
+        </Navbar>
+        </>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
