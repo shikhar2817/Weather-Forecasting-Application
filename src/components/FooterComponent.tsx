bec55762c21adf5b3399ec2d52adc1169d721052
Shikhar Singh <shikharsingh0076@gmail.com>
2021-01-26 17:06:39 +0530

    Added Author in footer

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -5,8 +5,9 @@ export function Footer() {
     <div className="footer dark jumbotron" style={{marginBottom:'0px', backgroundColor :'#343434', color:'floralwhite'}}>
         <div className="container">
             <div className="row justify-content-center">             
-                <div className="col-auto">
+                <div className="col-auto" style={{textAlign:'center'}} >
                     <p>© Copyright 2021 Weather Erudite. All Rights Reserved.</p>
+                    <p>Created and maintained by Shikhar Singh.</p>
                 </div>
             </div>
         </div>
