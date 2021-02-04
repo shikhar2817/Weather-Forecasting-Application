8b926a8492bbfda678daf37bbe20795de587adda
Shikhar Singh <shikharsingh0076@gmail.com>
2021-02-04 19:27:12 +0530

    Initial Icon setup

--- src/components/Tabs/NowTabComponent.tsx
+++ src/components/Tabs/NowTabComponent.tsx
@@ -1,10 +1,12 @@
 import './tabsStyles.css';
 import { Tab , Tabs, Col, Row, Container } from 'react-bootstrap';
+import { Sun } from '../../shared/WeatherIcons';
 
 export default function NowTab() {
     return (
         <div className="make-visible">
             Now Component
+            <Sun/>
         </div>
     );
 };
---
+++ src/shared/WeatherIcons.tsx
@@ -0,0 +1,18 @@
+const style = {
+    
+}
+
+export const Sun = () => {
+    return (
+        <div className="container">
+            <div className="weather-sun">
+                <div className="sun">
+                    <div className="rays"></div>
+                    <div className="rays"></div>
+                    <div className="rays"></div>
+                    <div className="rays"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
