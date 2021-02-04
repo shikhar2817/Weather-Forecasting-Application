d174df01057f1af2342e8c236247a69b28fc91f5
Shikhar Singh <shikharsingh0076@gmail.com>
2021-02-04 20:15:48 +0530

    Weather Icons Component Completed

--- src/components/Tabs/NowTabComponent.tsx
+++ src/components/Tabs/NowTabComponent.tsx
@@ -1,12 +1,12 @@
 import './tabsStyles.css';
 import { Tab , Tabs, Col, Row, Container } from 'react-bootstrap';
-import { Sun } from '../../shared/WeatherIcons';
+import { Sun, Snow, CloudAndSun, Clouds, Rain } from '../../shared/WeatherIcons';
 
 export default function NowTab() {
     return (
         <div className="make-visible">
             Now Component
-            <Sun/>
+            
         </div>
     );
 };
--- src/shared/WeatherIcons.tsx
+++ src/shared/WeatherIcons.tsx
@@ -1,6 +1,4 @@
-const style = {
-    
-}
+import './WeatherIcons.css';
 
 export const Sun = () => {
     return (
@@ -16,3 +14,56 @@ export const Sun = () => {
         </div>
     );
 }
+
+export const Snow = () => {
+    return (
+        <div className="container">
+            <div className="weather-snow">
+                <div className="snow">
+                    <div className="f"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export const CloudAndSun = () => {
+    return (
+        <div className="container">
+            <div className="weather-cloudAndSun">
+                <div className="cloud"></div>
+                <div className="sun">
+                    <div className="rays"></div>
+                    <div className="rays"></div>
+                    <div className="rays"></div>
+                    <div className="rays"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export const Clouds = () => {
+    return (
+        <div className="container">
+            <div className="weather-cloud">
+				<div className="cloud"></div>
+				<div className="cloud"></div>
+			</div>
+        </div>
+    );
+};
+
+export const Rain = () => {
+    return(
+        <div className="container">
+            <div className="weather-rain">
+				<div className="cloud"></div>
+				<div className="rain"></div>
+				<div className="rain"></div>
+				<div className="rain"></div>
+				<div className="rain"></div>
+			</div>
+        </div>
+    );
+};
