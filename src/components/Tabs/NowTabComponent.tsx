79ec7d98293c188a3a6dce8a709710bcb8f638db
Shikhar Singh <shikharsingh0076@gmail.com>
2021-02-08 00:38:15 +0530

    Now tab complete

--- src/components/Tabs/NowTabComponent.tsx
+++ src/components/Tabs/NowTabComponent.tsx
@@ -7,9 +7,7 @@ export default function NowTab(props: any) {
     console.log(props);
 
     const convertTimeStamp = (unixTimestamp: number) => {
-        let dateObj = new Date(unixTimestamp * 1000); 
-        let utcString = dateObj.toUTCString(); 
-        let time = utcString; 
+        let time = new Date(unixTimestamp * 1000).toLocaleTimeString("en-US");
         return time;
     }
 
@@ -22,53 +20,54 @@ export default function NowTab(props: any) {
                         {props.data.daily.data[0].summary} And also there is {props.data.daily.data[0].precipProbability * 100}% chance of rain.
                     </Col>
                 </Row>
-                <Row>
-                    <Col md={6} className="clean-box">
-                        High : {props.data.daily.data[0].temperatureHigh} °C
-                    </Col>
-                    <Col md={6} className="clean-box">
-                        Low : {props.data.daily.data[0].temperatureLow} °C
-                    </Col>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>High : </Col>
+                    <Col>{props.data.daily.data[0].temperatureHigh} °C</Col>
+                    <Col>Low : </Col>
+                    <Col>{props.data.daily.data[0].temperatureLow} °C</Col>
                 </Row>
-                <Row>
-                    <Col md={6} className="clean-box">
-                        Sunrise :  {convertTimeStamp(props.data.daily.data[0].sunriseTime)} , <Moment unix>{props.data.daily.data[0].sunriseTime}</Moment>
-                    </Col>
-                    <Col md={6} className="clean-box">
-                        Sunset : <Moment unix>{props.data.daily.data[0].sunsetTime}</Moment>
-                    </Col>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>Sunrise : </Col>
+                    <Col>{convertTimeStamp(props.data.daily.data[0].sunriseTime)}</Col>
+                    <Col>Sunset : </Col>
+                    <Col>{convertTimeStamp(props.data.daily.data[0].sunsetTime)}</Col>
                 </Row>
-                <Row>
-                    <Col md={6} className="clean-box">
-                        Wind :  {props.data.daily.data[0].windSpeed} m/sec
-                    </Col>
-                    <Col md={6} className="clean-box">
-                        Visibility : {props.data.daily.data[0].visibility} Km
-                    </Col>
+                
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>Dew Point : </Col>
+                    <Col> {props.data.daily.data[0].dewPoint}°C </Col>
+                    <Col>Visibility : </Col>
+                    <Col> {props.data.daily.data[0].visibility} Km </Col>
                 </Row>
-                <Row>
-                    <Col md={6} className="clean-box">
-                        UV Index :  {props.data.daily.data[0].uvIndex} out of 10
-                    </Col>
-                    <Col md={6} className="clean-box">
-                        Humidity : {Math.round(props.data.daily.data[0].humidity * 100)}% 
-                    </Col>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>Wind : </Col>
+                    <Col> {props.data.daily.data[0].windSpeed} m/sec </Col>
+                    <Col>Wind Direction : </Col>
+                    <Col> {props.data.daily.data[0].windBearing}° Km </Col>
                 </Row>
-                <Row>
-                    <Col md={6} className="clean-box">
-                        Pressure :  {props.data.daily.data[0].pressure} mb
-                    </Col>
-                    <Col md={6} className="clean-box">
-                        Moon Phase : {props.data.daily.data[0].moonPhase}
-                    </Col>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>Wind Gust : </Col>
+                    <Col> {props.data.daily.data[0].windGust} m/sec </Col>
+                    <Col>Wind Gust Time : </Col>
+                    <Col> {convertTimeStamp(props.data.daily.data[0].windGustTime)} </Col>
                 </Row>
-                <Row>
-                    <Col md={6} className="clean-box">
-                        Cloud Cover :  {props.data.daily.data[0].cloudCover * 100}%
-                    </Col>
-                    <Col md={6} className="clean-box">
-                        Ozone : {props.data.daily.data[0].ozone} DU
-                    </Col>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>UV Index : </Col>
+                    <Col> {props.data.daily.data[0].uvIndex} out of 10 </Col>
+                    <Col>Humidity : </Col>
+                    <Col> {Math.round(props.data.daily.data[0].humidity * 100)}% </Col>
+                </Row>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>Pressure : </Col>
+                    <Col> {props.data.daily.data[0].pressure} mb </Col>
+                    <Col>Moon Phase : </Col>
+                    <Col> {props.data.daily.data[0].moonPhase} </Col>
+                </Row>
+                <Row xs={2} md={4} className="clean-box">
+                    <Col>Cloud Cover : </Col>
+                    <Col> {props.data.daily.data[0].cloudCover * 100}% </Col>
+                    <Col>Ozone : </Col>
+                    <Col> {props.data.daily.data[0].ozone} DU </Col>
                 </Row>
             </Container>
         </div>
